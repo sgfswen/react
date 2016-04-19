6c51736344093e7c8519d13787f4143b7126af82
xiadd <1278609551@qq.com>
2016-04-19 15:44:56 +0800

    add gulp-concat and postcss

--- README.md
+++ README.md
@@ -1,16 +1,8 @@
-## 练习而已
-```python
-#coding=utf-8
-class Foo {
-  def __init__(self):
-    self.name='xiadd'
-
-  def set_name(self):
-    self.name='de'
-}
-
-if __name__ == '__main__':
-  foo=Foo()
-  foo()
-
+```
+app
+├── components
+│   ├── Comment.js
+│   ├── CommentForm.js
+│   └── CommentList.js
+└── index.js
 ```
---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+
+gulp.task('sass',function () {
+  return gulp.src("components/*/*.scss")
+            .pipe(sass({outputStyle:"expanded"}))
+            .pipe(gulp.dest('./dist/build'))
+})
+
+gulp.task('concat',['sass'],function () {
+  return gulp.src('dist/build/*/*.css')
+            .pipe(concat('main.css'))
+            .pipe(gulp.dest('dist/build'))
+})
+
+gulp.task('default',function () {
+  gulp.run('sass');
+  gulp.watch('components/*/*.scss',['concat'])
+})
