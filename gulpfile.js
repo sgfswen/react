fed1b2f7967ab7ad679a5f994d34c17182585dc5
xiadd <1278609551@qq.com>
2016-04-19 16:45:58 +0800

    add rev

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var rev = require('gulp-rev');
+var revCollector = require('gulp-rev-collector');
+var revReplace =require('gulp-rev-replace');
+var useref = require('gulp-useref');
 
 gulp.task('sass',function () {
   return gulp.src("components/*/*.scss")
@@ -14,6 +18,23 @@ gulp.task('concat',['sass'],function () {
             .pipe(gulp.dest('dist/build'))
 })
 
+gulp.task('rev',['replace'],function () {
+  return gulp.src('dist/build/main.css')
+            .pipe(rev())
+            .pipe(gulp.dest('dist/build/versions'))
+            .pipe(rev.manifest())
+            .pipe(gulp.dest('.'))
+})
+
+gulp.task('replace',function (){
+  var mainifest = gulp.src('./rev-manifest.json');
+
+  return gulp.src('dist/build/versions/*.css')
+            .pipe(revReplace({mainifest: mainifest}))
+            .pipe(gulp.dest('dist/build/versions'))
+})
+
+
 gulp.task('default',['concat'],function () {
-  gulp.watch('components/*/*.scss',['concat'])
+  gulp.watch('components/*/*.scss',['concat','rev'])
 })
